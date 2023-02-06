25cdf6037a1dcf29efbba1158f69fe97c9c011d1
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-02-06 15:38:49 -0300

    part-2

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 
 import model.entities.CarRental;
 import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
 
 public class Program {
 
@@ -15,7 +17,7 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:ss");
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		
 		System.out.println("Entre com os dados do aluhuel");
 		System.out.print("Modelo do carro: ");
@@ -27,6 +29,20 @@ public class Program {
 		
 		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
 		
+		System.out.print("Entre com o preço por hora: ");
+		double pricePerHour = sc.nextDouble();
+		System.out.print("Entre com o preço por dia: ");
+		double pricePerDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+		
+		rentalService.processInvoice(cr);
+		
+		System.out.println("FATURA:");
+		System.out.println("Pagamento básico: R$ " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+		System.out.println("Imposto: R$ " + String.format("%.2f", cr.getInvoice().getTax()));
+		System.out.println("Pagamento total: R$ " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
+
 		sc.close();
 	}
 
